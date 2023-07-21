58fbf4516491bf523c1fe84ce0633a613b8f182f
Lars Kecker <lars.kecker2009@gmail.com>
2023-07-21 17:23:03 +0200

    maximize material
    Pick the move that maximizes material advantage

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,51 @@
 ﻿using ChessChallenge.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class MyBot : IChessBot
 {
+    static readonly Dictionary<PieceType, int> chessPieceValues = new Dictionary<PieceType, int>
+    {
+        { PieceType.Pawn, 1 },
+        { PieceType.Knight, 3 },
+        { PieceType.Bishop, 3 },
+        { PieceType.Rook, 5 },
+        { PieceType.Queen, 9 },
+        { PieceType.King, 0 }
+    };
+
     public Move Think(Board board, Timer timer)
     {
+        bool playerIsWhite = board.IsWhiteToMove;
+
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+
+        int bestScore = int.MinValue;
+        Move bestMove = moves[0];
+
+        foreach (Move move in moves)
+        {
+            
+            board.MakeMove(move);
+            Board newBoard = new(board.board);
+
+            // evaluate position
+            PieceList[] pieceLists = newBoard.GetAllPieceLists();
+            int netScore = pieceLists.Sum(p => (p.IsWhitePieceList == playerIsWhite ? 1 : -1) * chessPieceValues[p.TypeOfPieceInList] * p.Count);
+            Console.WriteLine(string.Join(",", pieceLists.Select(p => chessPieceValues[p.TypeOfPieceInList])));
+
+            Console.WriteLine(move.ToString() + " " + netScore);
+            if (netScore > bestScore)
+            {
+                bestScore = netScore;
+                bestMove = move;
+            }
+
+            board.UndoMove(move);
+        }
+        Console.WriteLine("----------");
+
+        return bestMove;
     }
 }
