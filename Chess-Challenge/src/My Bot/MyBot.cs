363d48b0cf89b1e91d10c3283989d851761f40ff
Lars Kecker <lars.kecker2009@gmail.com>
2023-07-21 19:24:30 +0200

    Consider checkmate

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -17,7 +17,7 @@ public class MyBot : IChessBot
 
     public Move Think(Board board, Timer timer)
     {
-        int depth = 3;
+        int depth = 4;
         bool playerIsWhite = board.IsWhiteToMove;
 
         int bestValue = int.MinValue;
@@ -39,34 +39,25 @@ public class MyBot : IChessBot
 
         return bestMove;
     }
-    
+
     private int minmax(Board board, bool maximize, int depth)
     {
-        if (depth == 0)
+        Move[] moves = board.GetLegalMoves();
+        if (depth == 0 || moves.Length == 0)
         {
             return Evaluate(board);
         }
-        Move[] moves = board.GetLegalMoves();
 
-        int value = maximize ? int.MinValue : int.MaxValue;
+        int value = maximize ? int.MinValue + 1 : int.MaxValue;
 
         foreach (Move move in moves)
         {
             board.MakeMove(move);
-
             int moveValue = minmax(board, !maximize, depth - 1);
-
-            if (maximize)
-            {
-                value = Math.Max(value, moveValue);
-            } else
-            {
-                value = Math.Min(value, moveValue);
-            }
+            value = maximize ? Math.Max(value, moveValue) : Math.Min(value, moveValue);
             board.UndoMove(move);
-
         }
-        return value;        
+        return value;
 
     }
 
@@ -75,6 +66,10 @@ public class MyBot : IChessBot
      */
     private static int Evaluate(Board board)
     {
+        if (board.IsInCheckmate())
+        {
+            return board.IsWhiteToMove ? int.MinValue + 1 : int.MaxValue;
+        }
         Board newBoard = new Board(new ChessChallenge.Chess.Board(board.board));
         PieceList[] pieceLists = newBoard.GetAllPieceLists();
         int netScore = pieceLists.Sum(p => (p.IsWhitePieceList ? 1 : -1) * chessPieceValues[p.TypeOfPieceInList] * p.Count);
